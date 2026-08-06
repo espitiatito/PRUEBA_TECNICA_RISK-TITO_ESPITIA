5bbdd41e770e4f56e496e93eb0e8cd4c5523719a
jducuara <j.ducuara@riskgc.com>
2026-08-06 11:54:10 -0500

    detalle del cobro

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 
+import { ChargeDetail } from './charges/charge-detail/charge-detail';
 import { ChargesList } from './charges/charges-list/charges-list';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'charges', pathMatch: 'full' },
   { path: 'charges', component: ChargesList },
+  { path: 'charges/:id', component: ChargeDetail },
+  { path: '**', redirectTo: 'charges' },
 ];
---
+++ web/src/app/charges/charge-detail/charge-detail.ts
@@ -0,0 +1,44 @@
+import { DatePipe, DecimalPipe } from '@angular/common';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
+import {
+  ChargeAttempt,
+  ChargeDetail as ChargeDetailDto,
+  ETIQUETAS_ESTADO,
+  ETIQUETAS_MOTIVO,
+} from '../../models/charge';
+import { ChargesService } from '../../services/charges.service';
+
+@Component({
+  selector: 'app-charge-detail',
+  imports: [RouterLink, DatePipe, DecimalPipe],
+  templateUrl: './charge-detail.html',
+})
+export class ChargeDetail implements OnInit {
+  private readonly route = inject(ActivatedRoute);
+  private readonly charges = inject(ChargesService);
+
+  id = '';
+  cobro = signal<ChargeDetailDto | null>(null);
+  intentos = signal<ChargeAttempt[]>([]);
+
+  readonly etiquetasEstado = ETIQUETAS_ESTADO;
+  readonly etiquetasMotivo = ETIQUETAS_MOTIVO;
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id') ?? '';
+    this.cargar();
+  }
+
+  cargar(): void {
+    this.charges.get(this.id).subscribe((cobro) => {
+      this.cobro.set(cobro);
+
+      // el historial nos llegaba desordenado, lo pedimos aparte
+      this.charges.get(this.id).subscribe((detalle) => {
+        this.intentos.set(detalle.attempts);
+      });
+    });
+  }
+}
